f183250081f1ba4411dc76efdc8edbbb9e8b8c96
svjharris <svjharris@gmail.com>
2015-03-09 10:26:42 -0700

    Changes thanks to /u/maximinus-thrax

--- guitarNotePractice.py
+++ guitarNotePractice.py
@@ -1,53 +1,50 @@
+import sys, random as r
 
-import random as r
+def printTitle():
+    print "\n|-------------------------------|"
+    print "|Welcome to Guitar Note Practice|"
+    print "|-------------------------------|\n"
 
-notes = ['A','B','C','D','E','F','G']
-noteOfDay = notes[r.randint(0,6)]
-
-print "\n|-------------------------------|"
-print "|Welcome to Guitar Note Practice|"
-print "|-------------------------------|\n"
-
-print "The note of the day is " + noteOfDay +"\n"
-
-userInput = True
-
-print "I'll give you:"
-print " 1  A note in scientific pitch notation"
-print " 2  A note and the string on which to play it"
-print " 3  A new note of the day"
-print " 0  Quit\n"
-
-while(userInput):
-
-  try:
-    userInput = input( "Enter your selection:\n")
-
-    # shitty switchcase subsitute
-    if (userInput == 1):
-      print "\nPlay a " + noteOfDay + str(r.randint(1,4)) + "\n"
-    elif userInput == 2:
-      print "\nPlay a "+noteOfDay+" on the " + \
-             str(r.randint(1,6)) + " string\n" 
-    elif userInput == 3:
-      noteOfDay = notes[r.randint(0,6)]
-      print "\n Note of the day changed to " + noteOfDay + "\n"
-    elif userInput == 0:
-      print "\n Exiting\n" 
-    else:
-      print "\nInvalid entry, try again"
-      print "I'll give you:"
-      print " 1  A note in scientific notation"
-      print " 2  A note and the string on which to play it"
-      print " 3  A new note of the day"
-      print " 0  Quit\n"
-
-  except:
-    print "\nInvalid entry, try again"
+def printOptions():
     print "I'll give you:"
-    print " 1  A note in scientific notation"
-    print " 2  A note and the string on which to play it"
-    print " 3  A new note of the day"
-    print " 0  Quit\n"
+    print " 1 A note in scientific pitch notation"
+    print " 2 A note and the string on which to play it"
+    print " 3 A new note of the day"
+    print " 4 Some advice"
+    print " 0 Quit\n"
 
+def printNote():
+    print "The note of the day is {0}\n".format(noteOfDay)
 
+def printError():
+    print "\nInvalid entry, try again"
+    printOptions()
+
+def getNote():
+    return('ABCDEFG'[r.randint(0,6)])
+
+def getUserOptions():
+    printOptions()
+    note = getNote()
+    while(True):
+        try:
+            userInput = int(input( "Enter your selection:\n"))
+        except:
+            printError()
+        if(userInput == 1):
+            print "\nPlay a {0}{1}\n".format(note, str(r.randint(1,4)))
+        elif(userInput == 2):
+            print "\nPlay a {0} on the {1} string".format(note, str(r.randint(1,6)))
+        elif(userInput == 3):
+            note = getNote()
+            print "\n Note of the day changed to {0}\n".format(note)
+        elif(userInput == 4):
+            print "Listen to Jerry Garcia"
+        elif userInput == 0:
+            sys.exit()
+        else:
+            printError()
+
+printTitle()
+printNote()
+getUserOptions()
